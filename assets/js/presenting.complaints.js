2da3511f380cb9ebd0f2f78edcd5ff37a229f417
Mwatha Bwanali <mwathabwanali@gmail.com>
2019-01-31 20:52:58 +0200

    adding OPD presenting complaints

---
+++ assets/js/presenting.complaints.js
@@ -0,0 +1,213 @@
+var presentingComplaintsHash = {};
+
+function clearSelection(type_of_complaint) {
+  presentingComplaintsHash[type_of_complaint] = [];
+  buildPresentaingComplaints(type_of_complaint);
+}
+
+function buildPresentaingComplaints(type_of_complaint) {
+  var frame = document.getElementById('inputFrame' + tstCurrentPage);
+  frame.style = 'height: 90%; overflow: auto;';
+  frame.innerHTML = null;
+
+  getPresentingComplaints(type_of_complaint);
+  var clearButton = document.getElementById('clearButton');
+  clearButton.setAttribute('onmousedown',"clearSelection('" + type_of_complaint + "');");
+}
+
+function presentingComplaints(concept_sets, type_of_complaint) {
+  var frame = document.getElementById('inputFrame' + tstCurrentPage);
+  var main_container = document.createElement('div');
+  main_container.setAttribute('id','complaints-container');
+  frame.appendChild(main_container);
+  var row_count = 1;
+  var row;
+
+  concept_names = []
+  for(var i = 0 ; i < concept_sets.length; i++) {
+    if(concept_sets[i].name.toLowerCase() == 'other')
+      continue;
+
+    if(concept_sets[i].name.toLowerCase() == 'none')
+      continue;
+
+    concept_names.push(concept_sets[i].name);
+  }
+  
+  concept_names = concept_names.sort();
+  concept_names.push('Other');
+  concept_names.push('None');
+  
+  for(var x = 0 ; x < concept_names.length; x++){
+    for(var i = 0 ; i < concept_sets.length; i++) {
+      if(concept_names[x].toLowerCase() != concept_sets[i].name.toLowerCase())
+        continue;
+
+      if(row_count == 1){
+        row = document.createElement('div');
+        row.setAttribute('class','complaints-container-row');
+        main_container.appendChild(row);
+      }
+
+      cell = document.createElement('div');
+      cell.setAttribute('class','complaints-container-cell');
+      cell.innerHTML = concept_sets[i].name;
+      cell.setAttribute('selected', 'false');
+      cell.setAttribute('concept_id', concept_sets[i].concept_id);
+      cell.setAttribute('complaint-type', type_of_complaint);
+      cell.setAttribute('onmousedown','complaintClicked(this);');
+      row.appendChild(cell);
+    
+      row_count++;
+      if(row_count == 6)
+        row_count = 1;
+
+    }
+  }
+
+  autoHighLight(type_of_complaint);
+}
+
+function autoHighLight(type_of_complaint) {
+  var list = document.getElementsByClassName('complaints-container-cell');
+  var temp = presentingComplaintsHash[type_of_complaint];
+
+  if(temp == undefined)
+    return;
+
+  for(var i = 0 ; i < list.length ; i++){
+    for(var x = 0 ; x < temp.length ; x++){
+      if(list[i].getAttribute('concept_id') == temp[x]){
+        list[i].setAttribute('selected', 'true');
+        list[i].style = 'background-color: lightblue;';
+      }
+    }
+  }
+}
+
+function complaintClicked(e) {
+  var type_of_complaint = e.getAttribute('complaint-type');
+
+  if(e.getAttribute('selected') == 'false'){
+    e.setAttribute('selected', 'true');
+    e.style = 'background-color: lightblue;';
+    addToHash(type_of_complaint, e.getAttribute('concept_id'));    
+  }else{
+    e.setAttribute('selected', 'false');
+    e.style = 'background-color: "";';
+    removeFromHash(type_of_complaint, e.getAttribute('concept_id'));
+  }
+
+}
+
+function addToHash(key, concept_id) {
+  if(isHashEmpty(presentingComplaintsHash))
+    presentingComplaintsHash[key] = [];
+
+  try {
+    if(presentingComplaintsHash[key].indexOf(concept_id) < 0)
+      presentingComplaintsHash[key].push(concept_id);
+
+  }catch(e) {
+    presentingComplaintsHash[key] = [];
+    if(presentingComplaintsHash[key].indexOf(concept_id) < 0)
+      presentingComplaintsHash[key].push(concept_id);
+
+  }
+
+}
+
+function removeFromHash(key, concept_id) {
+  var temp = presentingComplaintsHash[key];
+  presentingComplaintsHash[key] = [];
+
+  for(var i = 0 ; i < temp.length ; i++){
+    if(temp[i] != concept_id)
+      presentingComplaintsHash[key].push(temp[i])
+  
+  }
+
+}
+
+function getPresentingComplaints(type_of_complaint) {
+  var complaint_concept_set = {};
+  complaint_concept_set['Specific presenting complaint'] = 8677;
+  complaint_concept_set['Presenting complaint'] = 8578;
+
+  var concept_set = complaint_concept_set[type_of_complaint];
+  var url = apiProtocol+'://'+apiURL+':'+apiPort+'/api/v1/concept_set';
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var objs = JSON.parse(this.responseText);
+      presentingComplaints(objs, type_of_complaint);
+    }
+  };
+  xhttp.open("GET", (url + "?id=" + concept_set + "&name="), true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send();
+}
+
+function prepareToSave() {
+  if(isHashEmpty(presentingComplaintsHash)) {
+    showMessage('No selection made. Please selection one or more complaints');
+    return;
+  }
+
+  var observations = [];
+  var keys = []
+  
+  for(key in presentingComplaintsHash) {
+    var concept_id = key == 'Presenting complaint' ? 8578 : 8677;
+    var temp = presentingComplaintsHash[key];
+    for(var i = 0 ; i < temp.length ; i++){
+      observations.push({concept_id: concept_id, value_coded: temp[i]})
+    }
+  }
+  
+  if(observations.length < 1) {
+    showMessage('No selection made. Please selection one or more complaints');
+    return;
+  }
+
+  var currentTime = moment().format(' HH:mm:ss');
+  var encounter_datetime = moment(sessionStorage.sessionDate).format('YYYY-MM-DD'); 
+  encounter_datetime += currentTime;
+                        	
+  var encounter = {
+    encounter_type_name: 'PRESENTING COMPLAINTS',
+    encounter_type_id:  122,
+    patient_id: sessionStorage.patientID,
+    encounter_datetime: encounter_datetime
+  }
+
+  submitParameters(encounter, "/encounters", "saveObs");
+}
+
+function saveObs(encounter) { 
+  
+  var observations = [];
+  var keys = []
+  
+  for(key in presentingComplaintsHash) {
+    var concept_id = key == 'Presenting complaint' ? 8578 : 8677;
+    var temp = presentingComplaintsHash[key];
+    for(var i = 0 ; i < temp.length ; i++){
+      observations.push({concept_id: concept_id, value_coded: temp[i]})
+    }
+  }
+  
+  var obs = {
+    encounter_id: encounter["encounter_id"],
+    observations: observations
+  }; 
+
+  submitParameters(obs, "/observations", "nextPage")  
+}
+
+function nextPage(obs){
+  nextEncounter(sessionStorage.patientID, sessionStorage.programID);
+}
+
